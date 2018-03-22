7509b97eb2bcfec9e061e202a21659b57bfaf53d
Matias Turunen <mt95@windowslive.com>
2018-03-22 17:17:29 +0200

    gps test

---
+++ gps_test.py
@@ -0,0 +1,36 @@
+import time
+import serial
+import pynmea2
+
+port = "/dev/ttyAMA0" # the serial port to which the pi is connected.
+ 
+#create a serial object
+ser = serial.Serial(port, baudrate = 9600, timeout = 0.5)
+
+while 1:
+	try:
+		data = ser.readline()
+		data = data.decode("utf-8")
+	except:
+		("loading") 
+		#wait for the serial port to churn out data
+	
+	print(data)
+ 
+	if data[0:6] == '$GPGGA': # the long and lat data are always contained in the GPGGA string of the NMEA data
+  
+		msg = pynmea2.parse(data)
+		print('msg')
+		#print(msg)
+
+		#parse the latitude and print
+		latval = msg.lat
+		concatlat = "lat:" + str(latval)
+		print (concatlat)
+ 
+		#parse the longitude and print
+		longval = msg.lon
+		concatlong = "long:"+ str(longval)
+		print (concatlong)
+		
+	time.sleep(0.3)#wait a little before picking the next data.
